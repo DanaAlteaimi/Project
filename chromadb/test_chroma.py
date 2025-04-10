5a61be071c92c8bb0b1c008235d77f9768067500
Ubuntu <azureuser@sda-project.mspbeumtlosenakwtkjb0435yg.bx.internal.cloudapp.net>
2025-04-10 07:21:54 +0000

    Initial commit with sample FastAPI app

--- chromadb/test_chroma.py
+++
@@ -1,9 +0,0 @@
-from chromadb import Client
-from chromadb.config import Settings  
-from chromadb import PersistentClient
-
-client = PersistentClient(path="./")
-
-collections = client.list_collections()
-print([c.name for c in collections])
-
